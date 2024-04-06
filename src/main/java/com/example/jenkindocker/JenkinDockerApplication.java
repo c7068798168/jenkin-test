bf116318ecc8be8f89cd4fb9f76b7661015f0a1a
xing liu <c7068798168@gmail.com>
2024-04-05 21:59:45 -0400

    ab

--- src/main/java/com/example/jenkindocker/JenkinDockerApplication.java
+++ src/main/java/com/example/jenkindocker/JenkinDockerApplication.java
@@ -2,9 +2,17 @@ package com.example.jenkindocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinDockerApplication {
+
+	@GetMapping("/test")
+	public String test() {
+		return "Test Xing Liu";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinDockerApplication.class, args);
